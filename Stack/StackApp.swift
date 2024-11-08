f5d0f1cb969e72c5f1c488a372b24846eb7ccee8
Cristian Rus <cristianrus4@gmail.com>
2024-11-08 13:53:09 +1300

    Initial Commit

---
+++ Stack/StackApp.swift
@@ -0,0 +1,17 @@
+//
+//  StackApp.swift
+//  Stack
+//
+//  Created by Cristian Rus on 8/11/24.
+//
+
+import SwiftUI
+
+@main
+struct StackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
